e89037024cb0472ca83252267024bb83c2ea7f4e
ml1150258 <mark.lehr@rcc.edu>
2015-03-18 09:53:51 -0700

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

---
+++ Lab/TruthTable_Partial/main.cpp
@@ -0,0 +1,47 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on March 18, 2015, 9:37 AM
+ * Purpose:  Determine if Table created in Class
+ *           agrees with logic of the computer
+ */
+
+//System Library
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare variables
+    bool x,y;
+    //Heading
+    cout<<"X Y !X !Y X&&Y X||Y X^Y X^Y^Y X^Y^X "
+        <<"!(X&&Y) !X||!Y !(X||Y) !X&&!Y"<<endl;
+    //Output the first row of the table
+    x=true;y=true;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<"  ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"   ";
+    cout<<(x&&y?'T':'F')<<"    ";
+    cout<<(x||y?'T':'F')<<" ";
+    cout<<endl;
+    //Output the second row of the table
+    y=false;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<"  ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"   ";
+    cout<<(x&&y?'T':'F')<<"    ";
+    cout<<(x||y?'T':'F')<<" ";
+    cout<<endl;
+    //Exit stage right!
+    return 0;
+}
+
